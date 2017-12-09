787bff4f22c1aca8596941017df919f93ead64b0
Wander <wander.dmneto@gmail.com>
2017-12-09 13:20:46 +1000

    Creating project

---
+++ src/main/java/com/pragma/pragmabrewery/PragmabreweryApplication.java
@@ -0,0 +1,12 @@
+package com.pragma.pragmabrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragmabreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PragmabreweryApplication.class, args);
+	}
+}
